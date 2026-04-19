51f5dc63fabb03cd9bf08433779698bc1a0f0885
Higor Abreu <higorabreu@id.uff.br>
2026-04-19 14:54:06 -0300

    initial files

---
+++ index.ts
@@ -0,0 +1,48 @@
+let NEXTORDERID = 1
+
+const menu = [
+    { name: "Margherita", price: 8 },
+    { name: "Pepperoni", price: 10 },
+    { name: "Hawaiian", price: 10 },
+    { name: "Veggie", price: 9 },
+];
+
+let cashInRegister = 100;
+
+const orderQueue = [];
+
+function addNewPizza(pizzaObj) {
+    menu.push(pizzaObj);
+}
+
+function placeOrder(pizzaName) {
+    const pizzaItem = menu.find((item) => item.name === pizzaName);
+
+    if (!pizzaItem) {
+        console.error(`${pizzaName} does not exist in the menu`)
+        return
+    }
+
+    cashInRegister += pizzaItem.price;
+    const orderObj = { id: NEXTORDERID++, ...pizzaItem, status: "ordered" };
+    orderQueue.push(orderObj);
+    return orderObj;
+}
+
+function completeOrder(orderId) {
+    const orderObj = orderQueue.find((item) => item.id === orderId);
+    if (orderObj) orderObj.status = "completed";
+    return orderObj;
+}
+
+addNewPizza({ name: "Chicken Bacon Ranch", price: 12 })
+addNewPizza({ name: "BBQ Chicken", price: 12 })
+addNewPizza({ name: "Spicy Sausage", price: 12 })
+
+placeOrder("Chicken Bacon Ranch")
+
+completeOrder(1)
+
+console.log("Menu:", menu);
+console.log("Cash in register:", cashInRegister);
+console.log("Order queue:", orderQueue);
