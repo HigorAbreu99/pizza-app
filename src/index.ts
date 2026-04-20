a6fbfef88c8bf3acd604291a449274108e53ae6e
Higor Abreu <higorabreu@id.uff.br>
2026-04-20 14:32:51 -0300

    learning about partial and omit

--- src/index.ts
+++ src/index.ts
@@ -1,58 +1,84 @@
-let NEXTORDERID = 1
+let NEXTORDERID = 1;
+let NEXTPIZZAID = 1;
 
 type Pizza = {
-    name: string,
-    price: number,
-}
+    id: number;
+    name: string;
+    price: number;
+};
 
 type Order = {
-    id: number,
-    pizza: Pizza,
-    status: string,
-}
+    id: number;
+    pizza: Pizza;
+    status: "ordered" | "completed";
+};
 
 const menu: Pizza[] = [
-    { name: "Margherita", price: 8 },
-    { name: "Pepperoni", price: 10 },
-    { name: "Hawaiian", price: 10 },
-    { name: "Veggie", price: 9 },
+    { id: NEXTPIZZAID++, name: "Margherita", price: 8 },
+    { id: NEXTPIZZAID++, name: "Pepperoni", price: 10 },
+    { id: NEXTPIZZAID++, name: "Hawaiian", price: 10 },
+    { id: NEXTPIZZAID++, name: "Veggie", price: 9 },
 ];
 
 let cashInRegister = 100;
 
 const orderQueue: Order[] = [];
 
-function addNewPizza(pizzaObj: Pizza) {
-    menu.push(pizzaObj);
+function addNewPizza(pizzaObj: Omit<Pizza, "id">): Pizza {
+    const newPizza = {
+        id: NEXTPIZZAID++,
+        ...pizzaObj,
+    };
+    menu.push(newPizza);
+    return newPizza;
 }
 
-function placeOrder(pizzaName: string) {
+function placeOrder(pizzaName: string): Order | undefined {
     const pizzaItem = menu.find((item) => item.name === pizzaName);
 
     if (!pizzaItem) {
-        console.error(`${pizzaName} does not exist in the menu`)
-        return
+        console.error(`${pizzaName} does not exist in the menu`);
+        return;
     }
 
     cashInRegister += pizzaItem.price;
-    const orderObj: Order = { id: NEXTORDERID++, pizza:pizzaItem, status: "ordered" };
+    const orderObj: Order = {
+        id: NEXTORDERID++,
+        pizza: pizzaItem,
+        status: "ordered",
+    };
     orderQueue.push(orderObj);
     return orderObj;
 }
 
-function completeOrder(orderId: number) {
+function completeOrder(orderId: number): Order | undefined {
     const orderObj = orderQueue.find((item) => item.id === orderId);
     if (orderObj) orderObj.status = "completed";
     return orderObj;
 }
 
-addNewPizza({ name: "Chicken Bacon Ranch", price: 12 })
-addNewPizza({ name: "BBQ Chicken", price: 12 })
-addNewPizza({ name: "Spicy Sausage", price: 12 })
+function getPizzaDetail(identifier: string | number): Pizza | undefined {
+    if (typeof identifier === "string") {
+        return menu.find(
+            (item) => item.name.toLowerCase === identifier.toLowerCase,
+        );
+    } else if (typeof identifier === "number") {
+        return menu.find((item) => item.id === identifier);
+    } else {
+        console.error("Invalid identifier type");
+    }
+}
 
-placeOrder("Chicken Bacon Ranch")
+addNewPizza({ name: "Chicken Bacon Ranch", price: 12 });
+addNewPizza({ name: "BBQ Chicken", price: 12 });
+addNewPizza({ name: "Spicy Sausage", price: 12 });
 
-completeOrder(1)
+placeOrder("Chicken Bacon Ranch");
+placeOrder("Pepperoni");
+completeOrder(1);
+placeOrder("Anchovy");
+placeOrder("Veggie");
+completeOrder(2);
 
 console.log("Menu:", menu);
 console.log("Cash in register:", cashInRegister);
